971b5a8509ed5efe3971fddbca1f98124de551b1
patrick brisbin <pbrisbin@gmail.com>
2020-08-07 09:30:39 -0400

    Rework record-metrics lambda
    - Drive everything from ENV, which defaults dev
    - Expect new /systems/metric endpoint, and forward anything it reports

--- lambda/record-metrics/main.py
+++ lambda/record-metrics/main.py
@@ -1,34 +1,56 @@
 import boto3
 import os
 import requests
+import json
 
-ENV = os.environ['ENV'] # required
+ENV = os.environ.get('ENV', 'dev')
+ENV_DIMENSION = {'Name': 'Environment', 'Value': ENV}
 
 ssm = boto3.client('ssm')
-api_token_parameter_name = "/restyled/%s/restyled-api-token" % ENV
-api_token_parameter = ssm.get_parameter(Name=api_token_parameter_name)
-
-RESTYLED_API_HOST = os.environ.get('RESTYLED_API_HOST', 'https://restyled.io')
-RESTYLED_API_TOKEN = api_token_parameter['Parameter']['Value']
-
-RESTYLED_API_URL = "%s%s" % (RESTYLED_API_HOST, '/admin/machines/state')
-RESTYLED_API_HEADERS = {
-    'Accept': 'application/json',
-    'Content-type': 'application/json',
-    'Authorization': "token %s" % RESTYLED_API_TOKEN
-}
-
 cw = boto3.client('cloudwatch')
 
+
 def handler(_event, _context):
-    resp = requests.get(RESTYLED_API_URL, headers=RESTYLED_API_HEADERS)
-    depth = resp.json()['queueDepth']
-    cw.put_metric_data(
-        Namespace='Restyled',
-        MetricData=[{
-            'MetricName': 'QueueDepth',
-            'Dimensions': [{'Name': 'Environment', 'Value': ENV}],
-            'Value': depth
-        }]
-    )
-    return {'ok': True, 'depth': depth}
+    host = get_restyled_host(ENV)
+    token = get_restyled_token(ENV)
+
+    metrics = get_system_metrics(host, token)
+    metric_data = list(map(add_env_dimension, metrics))
+    cw.put_metric_data(Namespace='Restyled', MetricData=metric_data)
+
+    metric_names = list(map(lambda x: x['MetricName'], metric_data))
+    return {'ok': True, 'env': ENV, 'host': host, 'recorded': metric_names}
+
+
+def get_restyled_host(env):
+    overrides = {'dev': 'https://restyled.ngrok.io',
+                 'prod': 'https://restyled.io'}
+    return overrides.get(env, "https://%s.restyled.io" % env)
+
+
+def get_restyled_token(env):
+    token_parameter_name = "/restyled/%s/restyled-api-token" % env
+    token_parameter = ssm.get_parameter(Name=token_parameter_name)
+    return token_parameter['Parameter']['Value']
+
+
+def get_system_metrics(host, token):
+    resp = requests.get("%s/system/metrics" % host,
+                        params={'since-minutes': 1},
+                        headers={
+                            'Accept': 'application/json',
+                            'Content-type': 'application/json',
+                            'Authorization': "token %s" % token
+                        })
+    return resp.json()['metrics']
+
+
+def add_env_dimension(metric):
+    metric['Dimensions'] = metric.get('Dimensions', [])
+    metric['Dimensions'].append(ENV_DIMENSION)
+    return metric
+
+
+if __name__ == '__main__':
+    result = handler(None, None)
+    print(json.dumps(result, indent=2))
